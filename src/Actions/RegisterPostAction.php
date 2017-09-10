ab60008fcc9af01a3e196150a6f7b0d645073738
b4stet <ptitgnu404@gmail.com>
2017-09-10 16:24:00 +0200

    extracted user input validation from registration call.

--- src/Actions/RegisterPostAction.php
+++ src/Actions/RegisterPostAction.php
@@ -16,26 +16,45 @@ class RegisterPostAction extends AbstractAction{
 	    $password = $data['password'];
 	    $userTable = new UserTable($this->service['db']);
 
-	    if (empty($password)){
-	        $msg = "* password cannot be empty";
-	        $response = $this->service['view']->render($response, "register.html", ['msgPasswordError'=>$msg]);
-	    }elseif ($userTable->isExistUsername($username)){
-	        $msg = "* user already exists";
-	        $response = $this->service['view']->render($response, "register.html", ['msgUsernameError'=>$msg]);
-	    }else{
-	        if (preg_match('/^[a-zA-Z0-9]+$/',$username) === 1){
+	    $isValid = $this->isValidUsernameAndPassword($username,$password,$userTable);
+	    if ($isValid[0] === true){
 	            $user = new User();
 	            $user->setUsername($username);
 	            $user->setPassword($password);
 	            $userTable->save($user);
-	            $msg = "Account successfully registered. You can now log in.";
-	            $response = $this->service['view']->render($response, "register.html", ['msgRegisterSuccess'=>$msg]);    
-	        }else{
-	            $msg = "* username can only contain alphanumeric characters ([a-zA-Z0-9])";
-	            $response = $this->service['view']->render($response, "register.html", ['msgUsernameError'=>$msg]);
-	        }
 	    }
 
-	    return $response;
+	    return $response = $this->service['view']->render($response, "register.html", $isValid[1]);
+	}
+
+	public function isValidUsernameAndPassword($username,$password,$userTable){
+		$msg = [];
+		$res = true;
+
+		//username alphanumeric only
+		if (!(preg_match('/^[a-zA-Z0-9]+$/',$username) === 1)){
+            $msg['msgUsernameError'] = "* Username can only contain alphanumeric characters ([a-zA-Z0-9])";
+            $res = false;
+		}
+
+		//user not already exists
+		if ($userTable->isExistUsername($username)){
+	        $msg['msgUsernameError'] = "* User already exists";
+	        $res = false;
+	    }
+
+		//password not empty
+		if (empty($password)){
+	        $msg['msgPasswordError'] = "* Password cannot be empty";
+	        $res = false;
+		}
+
+		//set success msg
+		if($res === true){
+			$msg['msgRegisterSuccess'] = "Account successfully registered. You can now log in.";
+
+		}
+
+		return [$res,$msg];
 	}
 }
